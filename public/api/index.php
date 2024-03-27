243f35326193ca67ad33439473610db03d661cde
Chris Wilson <chris@legendserver.info>
2024-03-27 18:39:12 -0400

    Fix api output

--- public/api/index.php
+++ public/api/index.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Return a random setup and punchline
     $jokes = json_decode(file_get_contents(dirname(__DIR__).'/../public/assets/dadJokes.json'), true);
-    $response = json_encode($jokes[array_rand($jokes)]);
+    $response = json_encode($jokes['jokes'][array_rand($jokes['jokes'])]);
     header('Content-Type: application/json');
     header('Content-Length: '.strlen($response));
     echo $response;
